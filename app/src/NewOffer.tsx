e1af396aed29e569fe69353ed8b31ee20b5f0f41
rostislavlitovkin <rostislavlitovkin@gmail.com>
2023-06-10 14:34:24 +0200

    Compulsary wallet connection

--- app/src/NewOffer.tsx
+++ app/src/NewOffer.tsx
@@ -2,6 +2,7 @@ import { ChangeEvent, FormEvent, useState } from "react";
 import { Connect } from "./components/Connect";
 
 import "./NewOffer.css"
+import { useAccount } from "wagmi";
 
 interface FormValues {
     title: string;
@@ -15,6 +16,8 @@ interface FormValues {
 
 export function NewOffer() {
 
+    const { connector, isConnected } = useAccount()
+
     const [values, setValues] = useState<FormValues>({
         title: '',
         event: '',
@@ -23,16 +26,16 @@ export function NewOffer() {
         location: '',
         description: '',
         stake: 0,
-      });
-    
-      const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+    });
+
+    const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
         setValues({ ...values, [event.target.name]: event.target.value });
-      };
-    
-      const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+    };
+
+    const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
         event.preventDefault();
         console.log(values);
-      };
+    };
 
     return (
         <>
@@ -41,78 +44,85 @@ export function NewOffer() {
                     <Connect />
                     <h1>Hacker Packer Dao</h1>
 
-                    <div style={{fontWeight: 900}}>Offer / Ask</div>
-                    <br />
-                    <input
-                        className="input"
-                        type="text"
-                        name="title"
-                        placeholder="Title"
-                        value={values.title}
-                        onChange={handleChange}
-                        required
-                    />
-                    <input
-                        className="input"
-                        type="text"
-                        name="event"
-                        placeholder="Event"
-                        value={values.event}
-                        onChange={handleChange}
-                        required
-                    />
-                    <input
-                        className="input"
-                        type="date"
-                        name="from_date"
-                        placeholder="From"
-                        value={values.from_date}
-                        onChange={handleChange}
-                        required
-                    />
-                    <input
-                        className="input"
-                        type="date"
-                        name="till_date"
-                        placeholder="Till"
-                        value={values.till_date}
-                        onChange={handleChange}
-                        required
-                    />
-                     <input
-                        className="input"
-                        type="text"
-                        name="location"
-                        placeholder="Location"
-                        value={values.location}
-                        onChange={handleChange}
-                        required
-                    />
-                    <textarea
-                        className="input"
-                        name="description"
-                        placeholder="Description"
-                        value={values.description}
-                        onChange={(event) => {
-                            setValues({ ...values, [event.target.name]: event.target.value });
-                          }
-                        }
-                        required
-                    />
+                    {isConnected ?
+                        <>
+                            <div style={{ fontWeight: 900 }}>Offer / Ask</div>
+                            <br />
+                            <input
+                                className="input"
+                                type="text"
+                                name="title"
+                                placeholder="Title"
+                                value={values.title}
+                                onChange={handleChange}
+                                required
+                            />
+                            <input
+                                className="input"
+                                type="text"
+                                name="event"
+                                placeholder="Event"
+                                value={values.event}
+                                onChange={handleChange}
+                                required
+                            />
+                            <input
+                                className="input"
+                                type="date"
+                                name="from_date"
+                                placeholder="From"
+                                value={values.from_date}
+                                onChange={handleChange}
+                                required
+                            />
+                            <input
+                                className="input"
+                                type="date"
+                                name="till_date"
+                                placeholder="Till"
+                                value={values.till_date}
+                                onChange={handleChange}
+                                required
+                            />
+                            <input
+                                className="input"
+                                type="text"
+                                name="location"
+                                placeholder="Location"
+                                value={values.location}
+                                onChange={handleChange}
+                                required
+                            />
+                            <textarea
+                                className="input"
+                                name="description"
+                                placeholder="Description"
+                                value={values.description}
+                                onChange={(event) => {
+                                    setValues({ ...values, [event.target.name]: event.target.value });
+                                }
+                                }
+                                required
+                            />
 
-                    <input
-                        className="input"
-                        type="number"
-                        name="stake"
-                        placeholder="Stake amount"
-                        value={values.stake}
-                        onChange={handleChange}
-                        required
-                    />
+                            <input
+                                className="input"
+                                type="number"
+                                name="stake"
+                                placeholder="Stake amount"
+                                value={values.stake}
+                                onChange={handleChange}
+                                required
+                            />
 
-                    <button className="button" type="submit">
-                        Submit
-                    </button>
+                            <button className="button" type="submit">
+                                Submit
+                            </button>
+                        </>
+                        :
+                        <div style={{ margin: "auto" }}>
+                            Connect your wallet please
+                        </div>}
                 </form>
             </div>
         </>
