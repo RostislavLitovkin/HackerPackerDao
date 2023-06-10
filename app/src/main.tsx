739ca61c5fb7efcb6e53272918a419345c529319
rostislavlitovkin <rostislavlitovkin@gmail.com>
2023-06-10 17:12:53 +0200

    OfferDetailPage added

--- app/src/main.tsx
+++ app/src/main.tsx
@@ -10,6 +10,8 @@ import {
   RouterProvider,
 } from "react-router-dom";
 import { NewOffer } from './NewOffer'
+import { OfferDetail } from './OfferDetail'
+import "./main.css"
 
 const router = createBrowserRouter([
   {
@@ -19,6 +21,10 @@ const router = createBrowserRouter([
   {
     path: "/newoffer",
     element: <NewOffer />
+  },
+  {
+    path: "/offerdetail",
+    element: <OfferDetail />
   },
 ]);
 
