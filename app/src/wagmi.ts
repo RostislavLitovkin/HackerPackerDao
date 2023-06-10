686c0dc941e4deb421283052a4370396f4486aa8
rostislavlitovkin <rostislavlitovkin@gmail.com>
2023-06-10 13:04:04 +0200

    Hacker Packer title

--- app/src/wagmi.ts
+++ app/src/wagmi.ts
@@ -19,24 +19,10 @@ const { chains, publicClient, webSocketPublicClient } = configureChains(
 export const config = createConfig({
   autoConnect: true,
   connectors: [
-    new MetaMaskConnector({ chains }),
-    new CoinbaseWalletConnector({
-      chains,
-      options: {
-        appName: 'wagmi',
-      },
-    }),
     new WalletConnectConnector({
       chains,
       options: {
         projectId: walletConnectProjectId,
-      },
-    }),
-    new InjectedConnector({
-      chains,
-      options: {
-        name: 'Injected',
-        shimDisconnect: true,
       },
     }),
   ],
