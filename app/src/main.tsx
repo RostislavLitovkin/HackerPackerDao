ce8fc9e6c96050a09bf0272ef3d5a7d07b482345
rostislavlitovkin <rostislavlitovkin@gmail.com>
2023-06-10 14:29:46 +0200

    NewOfferPage added

--- app/src/main.tsx
+++ app/src/main.tsx
@@ -5,10 +5,27 @@ import { WagmiConfig } from 'wagmi'
 import { App } from './App'
 import { config } from './wagmi'
 
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import { NewOffer } from './NewOffer'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />
+  },
+  {
+    path: "/newoffer",
+    element: <NewOffer />
+  },
+]);
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <WagmiConfig config={config}>
-      <App />
+      <RouterProvider router={router} />
     </WagmiConfig>
   </React.StrictMode>,
 )
