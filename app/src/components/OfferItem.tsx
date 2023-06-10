d5b338df69c0bf7f59b409dd8063afcb06789f45
Petr Antonin <ad.wolf6@seznam.cz>
2023-06-10 17:22:19 +0200

    FrontEnd Bejby

---
+++ app/src/components/OfferItem.tsx
@@ -0,0 +1,47 @@
+import './OfferItemCss.css'
+
+export function OfferItem() {
+
+    const event = 'ETHPrague'
+    const title = 'Highshool student looking for a place to stay'
+    const dayFrom = '9'
+    const dayTo = '11'
+    const month = 'June'
+    const year = '2023'
+    const location = 'Prague, Czechia'
+    const description = 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Nulla non arcu lacinia neque faucibus fringilla. Maecenas ipsum velit, consectetuer eu lobortis ut, dictum at dui. Vivamus ac leo pretium faucibus. Quisque tincidunt scelerisque libero. In enim a arcu imperdiet malesuada. In convallis. Praesent id justo in neque elementum ultrices. In convallis. Duis viverra diam non justo. Nulla non arcu lacinia neque faucibus fringilla.'
+    const wallet = 'Wallet Address: '
+    const walletAddress = '0x34598234bkjj34592u34bivefu9ufvasdu9v'
+    const twitter = 'Twitter: '
+    const twitterAddress = 'https://twitter.com/elonmusk?lang=cs'
+    const stakeAmount = '1'
+    const stakeCrypto = 'ETH'
+    const trustAmount = '180'
+    const trust = 'TRUST'
+
+    return(
+        <div>
+            <div className="row">
+                    <div className="column-side">
+                        <h2><b>{event}</b></h2>
+                        <p><b>{dayFrom} - {dayTo} {month} {year}</b></p>
+                        <p><b>{location}</b></p>
+                    </div>
+                    <div className="column-middle">   
+                    <h2><b>{title}</b></h2> 
+                    <p>{description}</p>
+                    <p>{wallet}{walletAddress}</p>
+                    <p>{twitter}{twitterAddress}</p>
+                    </div>
+                    <div className="column-side">
+                    <h2><b>Stake:</b></h2> 
+                    <h2><b>{stakeAmount} {stakeCrypto}</b></h2> 
+                    <h2><b>{trustAmount} {trust}</b></h2> 
+                    <h2><button>Add Trust</button></h2> 
+                    <h2><button>Accept</button></h2>
+                    </div>
+                
+            </div>
+        </div>
+    );
+}
--- app/src/main.tsx
+++ app/src/main.tsx
@@ -3,12 +3,14 @@ import * as ReactDOM from 'react-dom/client'
 import { WagmiConfig } from 'wagmi'
 
 import { App } from './App'
+import { Dashboard } from './Dashboard'
 import { config } from './wagmi'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <WagmiConfig config={config}>
       <App />
+      <Dashboard/>
     </WagmiConfig>
   </React.StrictMode>,
 )
