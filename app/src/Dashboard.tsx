8e15fd655c12eba84329455abf94823c69e41799
LufyCZ <lufy@sushi.com>
2023-06-11 12:01:24 +0000

    fix: rules of hooks

--- app/src/Dashboard.tsx
+++ app/src/Dashboard.tsx
@@ -1,66 +1,65 @@
-import { OfferItem } from './components/OfferItem'
-import './DashboardCss.css'
-import { Header } from './components/Header'
-import { useContractRead } from 'wagmi'
-import abi from "./abi/abi.json"
+import { OfferItem } from "./components/OfferItem";
+import "./DashboardCss.css";
+import { Header } from "./components/Header";
+import {
+  Address,
+  useContractInfiniteReads,
+  useContractRead,
+  useContractReads,
+} from "wagmi";
+import { hackbackerAbi } from "./abi/hackbacker";
+import { BigNumber } from "ethers";
 
-function dashboardLength() {
+function useDashboardLength() {
   const { data, isError, isLoading } = useContractRead({
-    address: '0x826b3A6F625da5CF904D9E8cCf8817AB89d4899a',
-    abi: abi,
-    functionName: 'escrowLength',
-  })
+    address: "0x826b3A6F625da5CF904D9E8cCf8817AB89d4899a",
+    abi: hackbackerAbi,
+    functionName: "escrowLength",
+  });
 
-  return data
+  return data;
 }
 
-function dashboardEscrow(index: number) {
-  const { data, isError, isLoading } = useContractRead({
-    address: '0x826b3A6F625da5CF904D9E8cCf8817AB89d4899a',
-    abi: abi,
-    functionName: 'escrows',
-    args: [index]
-  })
+function useDashboardEscrows(length: number | undefined) {
+  const { data, isError, isLoading } = useContractReads({
+    contracts: new Array(length).fill(0).map(
+      (_, index) =>
+        ({
+          address: "0x826b3A6F625da5CF904D9E8cCf8817AB89d4899a" as Address,
+          abi: hackbackerAbi,
+          functionName: "escrows",
+          args: [BigInt(index)] as const,
+        } as const)
+    ),
+    enabled: !!length,
+    allowFailure: false,
+  });
 
-  console.log("Fetching")
-  return data
+  return data;
 }
 
 export function Dashboard() {
-
-  const length = parseInt(dashboardLength() + "")
-
-  console.log(length)
-
-  let d: any[] = []
-  for (var i = 0; i < length; i++) {
-    d.push(dashboardEscrow(i))
-  }
-
-  console.log(d)
+  const escrowLength = useDashboardLength();
+  const escrows = useDashboardEscrows(Number(escrowLength));
 
   return (
     <>
       <Header />
 
-      { /*<div style={{ display: "flex", justifyContent: "space-evenly" }}>
+      {/*<div style={{ display: "flex", justifyContent: "space-evenly" }}>
         <button onClick={() => {}}>Your Agreements</button>
         <button onClick={() => {}}>Disputes</button>
         <button onClick={() => {}}>Add New Request</button>
-  </div> */ }
+  </div> */}
 
-
-
-      {
-        d.map((thing: any, index) => {
-          console.log("THing: " + thing)
-          return <div className='dashboard' key={index}>
+      {escrows?.map((thing: any, index) => {
+        console.log("THing: " + thing);
+        return (
+          <div className="dashboard" key={index}>
             <OfferItem key={index} data={thing} />
           </div>
-        })
-      }
-
-
+        );
+      })}
     </>
-  )
+  );
 }
